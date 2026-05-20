5c2a42a94fd9d62dbd6e9ef010c12f2bee6fd8b8
Anisur Rahman <anisrahman.contact@gmail.com>
2026-05-20 21:32:44 +0600

    chore(matches): admin update (2 matches)

--- matches.js
+++ matches.js
@@ -15,5 +15,22 @@ const MATCHES_DATA = [
       name: "Roma",
       logo: "logos/roma.png"
     }
+  },
+  {
+    id: 2,
+    league: "FIFA World Cup 2026",
+    time: "3:00 AM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    link: "https://t.me",
+    homeTeam: {
+      name: "France",
+      logo: "logos/france.png"
+    },
+    awayTeam: {
+      name: "Serbia",
+      logo: "logos/serbia.png"
+    }
   }
 ];
