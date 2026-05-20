937d0fb069fb9b870e324ddb86af527452c19756
Anisur Rahman <anisrahman.contact@gmail.com>
2026-05-20 21:59:21 +0600

    chore(matches): admin update (1 matches)

--- matches.js
+++ matches.js
@@ -1,56 +1,5 @@
 const MATCHES_DATA = [
   {
-    id: 1,
-    league: "Italy, Serie A",
-    time: "2:15 AM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    link: "https://t.me",
-    homeTeam: {
-      name: "Juventus",
-      logo: "logos/juventus.png"
-    },
-    awayTeam: {
-      name: "Roma",
-      logo: "logos/roma.png"
-    }
-  },
-  {
-    id: 2,
-    league: "FIFA World Cup 2026",
-    time: "3:00 AM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    link: "https://t.me",
-    homeTeam: {
-      name: "France",
-      logo: "logos/france.png"
-    },
-    awayTeam: {
-      name: "Serbia",
-      logo: "logos/serbia.png"
-    }
-  },
-  {
-    id: 3,
-    league: "UEFA Champions League",
-    time: "3:00 AM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    link: "https://t.me",
-    homeTeam: {
-      name: "Tottenham Hotspur",
-      logo: "logos/tottenham.png"
-    },
-    awayTeam: {
-      name: "Liverpool",
-      logo: "logos/liverpool.png"
-    }
-  },
-  {
     id: 4,
     league: "FIFA World Cup 2026",
     time: "2:45 AM",
