b6123c5a35c8a99c0e02922a516b640f8d3822de
Anisur Rahman <anisrahman.contact@gmail.com>
2026-05-20 21:31:23 +0600

    chore(matches): admin update (1 matches)

--- matches.js
+++ matches.js
@@ -1,2 +1,19 @@
 const MATCHES_DATA = [
+  {
+    id: 1,
+    league: "Italy, Serie A",
+    time: "",
+    status: "",
+    day: "today",
+    isImportant: true,
+    link: "https://t.me",
+    homeTeam: {
+      name: "Juventus",
+      logo: "logos/juventus.png"
+    },
+    awayTeam: {
+      name: "Roma",
+      logo: "logos/roma.png"
+    }
+  }
 ];
