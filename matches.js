a397d85b29feb6b0f691b6818cd176964cc01c55
Anisur Rahman <anisrahman.contact@gmail.com>
2026-05-21 15:05:15 +0600

    chore(matches): admin update (7 matches)

--- matches.js
+++ matches.js
@@ -15,5 +15,107 @@ const MATCHES_DATA = [
       name: "Argentina",
       logo: "logos/argentina.png"
     }
+  },
+  {
+    id: 2,
+    league: "Copa America",
+    time: "3:00 AM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["https://t.me"],
+    homeTeam: {
+      name: "Uruguay",
+      logo: "logos/uruguay.png"
+    },
+    awayTeam: {
+      name: "Colombia",
+      logo: "logos/colombia.png"
+    }
+  },
+  {
+    id: 3,
+    league: "UEFA Champions League",
+    time: "3:00 AM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["https://t.me"],
+    homeTeam: {
+      name: "Crystal Palace",
+      logo: "logos/crystal-palace.png"
+    },
+    awayTeam: {
+      name: "Eintracht Frankfurt",
+      logo: "logos/eintracht-frankfurt.png"
+    }
+  },
+  {
+    id: 4,
+    league: "Spain, La Liga",
+    time: "",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["https://t.me"],
+    homeTeam: {
+      name: "Girona",
+      logo: "logos/girona.png"
+    },
+    awayTeam: {
+      name: "Real Oviedo",
+      logo: "logos/real-oviedo.png"
+    }
+  },
+  {
+    id: 5,
+    league: "Club Friendly",
+    time: "",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["https://t.me"],
+    homeTeam: {
+      name: "Manchester United",
+      logo: "logos/man-united.png"
+    },
+    awayTeam: {
+      name: "Everton",
+      logo: "logos/everton.png"
+    }
+  },
+  {
+    id: 6,
+    league: "Spain, La Liga",
+    time: "3:00 AM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["https://t.me"],
+    homeTeam: {
+      name: "Getafe",
+      logo: "logos/getafe.png"
+    },
+    awayTeam: {
+      name: "Rayo Vallecano",
+      logo: "logos/rayo-vallecano.png"
+    }
+  },
+  {
+    id: 7,
+    league: "England, English Premier League",
+    time: "3:00 AM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["https://t.me"],
+    homeTeam: {
+      name: "Fulham",
+      logo: "logos/fulham.png"
+    },
+    awayTeam: {
+      name: "Crystal Palace",
+      logo: "logos/crystal-palace.png"
+    }
   }
 ];
