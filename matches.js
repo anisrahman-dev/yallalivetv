1eb2faee260af2c1d1fa3973734ac04d7e5336e0
Anisur Rahman <anisrahman.contact@gmail.com>
2026-05-21 12:00:39 +0600

    chore(matches): admin update (0 matches)

--- matches.js
+++ matches.js
@@ -1,19 +1,2 @@
 const MATCHES_DATA = [
-  {
-    id: 5,
-    league: "FIFA World Cup 2026",
-    time: "11:45 AM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    link: "https://t.me",
-    homeTeam: {
-      name: "Denmark",
-      logo: "logos/denmark.png"
-    },
-    awayTeam: {
-      name: "Czech Republic",
-      logo: "logos/czech-republic.png"
-    }
-  }
 ];
