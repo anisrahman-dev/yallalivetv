2ee9131329191288dfddc456e61904ce093e199c
Anisur Rahman <anisrahman.contact@gmail.com>
2026-05-24 11:37:12 +0600

    chore(matches): admin update (6 matches)

--- matches.js
+++ matches.js
@@ -1,19 +1,104 @@
 const MATCHES_DATA = [
   {
     id: 1,
-    league: "Italy, Serie A",
-    time: "10:00 AM",
+    league: "England, English Premier League",
+    time: "9:00 PM",
     status: "",
     day: "today",
     isImportant: true,
-    servers: ["#"],
+    servers: [],
     homeTeam: {
-      name: "Bologna",
-      logo: "logos/bologna.png"
+      name: "Manchester United",
+      logo: "logos/man-united.png"
     },
     awayTeam: {
-      name: "Inter Milan",
-      logo: "logos/inter-milan.png"
+      name: "Brighton & Hove Albion",
+      logo: "logos/brighton.png"
+    }
+  },
+  {
+    id: 2,
+    league: "England, English Premier League",
+    time: "9:00 PM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["https://t.me"],
+    homeTeam: {
+      name: "Crystal Palace",
+      logo: "logos/crystal-palace.png"
+    },
+    awayTeam: {
+      name: "Arsenal",
+      logo: "logos/arsenal.png"
+    }
+  },
+  {
+    id: 3,
+    league: "England, English Premier League",
+    time: "9:00 PM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["https://t.me"],
+    homeTeam: {
+      name: "Fulham",
+      logo: "logos/fulham.png"
+    },
+    awayTeam: {
+      name: "Newcastle United",
+      logo: "logos/newcastle.png"
+    }
+  },
+  {
+    id: 4,
+    league: "England, English Premier League",
+    time: "9:00 PM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["https://t.me"],
+    homeTeam: {
+      name: "Liverpool",
+      logo: "logos/liverpool.png"
+    },
+    awayTeam: {
+      name: "Brentford",
+      logo: "logos/brentford.png"
+    }
+  },
+  {
+    id: 5,
+    league: "England, English Premier League",
+    time: "9:00 PM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["https://t.me"],
+    homeTeam: {
+      name: "Manchester City",
+      logo: "logos/man-city.png"
+    },
+    awayTeam: {
+      name: "Aston Villa",
+      logo: "logos/aston-villa.png"
+    }
+  },
+  {
+    id: 6,
+    league: "England, English Premier League",
+    time: "9:00 PM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["https://t.me"],
+    homeTeam: {
+      name: "Tottenham Hotspur",
+      logo: "logos/tottenham.png"
+    },
+    awayTeam: {
+      name: "Everton",
+      logo: "logos/everton.png"
     }
   }
 ];
