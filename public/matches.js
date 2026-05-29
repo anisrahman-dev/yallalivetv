3eb64ec806e58deb10a56dc9c5c4a867f5a63d7f
Anisur Rahman <anisrahman.contact@gmail.com>
2026-05-29 20:49:27 +0600

    chore(matches): admin update (1 matches)

--- public/matches.js
+++ public/matches.js
@@ -1,104 +1,19 @@
 const MATCHES_DATA = [
   {
     id: 1,
-    league: "England, English Premier League",
-    time: "9:00 PM",
+    league: "UEFA Champions League",
+    time: "10:00 PM",
     status: "",
     day: "today",
     isImportant: true,
-    servers: ["https://www.livelist.live/matches/%d8%a8%d8%b1%d8%a7%d9%8a%d8%aa%d9%88%d9%86-%d9%88-%d9%85%d8%a7%d9%86%d8%b4%d8%b3%d8%aa%d8%b1-%d9%8a%d9%88%d9%86%d8%a7%d9%8a%d8%aa%d8%af/"],
+    servers: ["https://t.me"],
     homeTeam: {
-      name: "Manchester United",
-      logo: "logos/man-united.png"
-    },
-    awayTeam: {
-      name: "Brighton & Hove Albion",
-      logo: "logos/brighton.png"
-    }
-  },
-  {
-    id: 2,
-    league: "England, English Premier League",
-    time: "9:00 PM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    servers: ["https://www.livelist.live/matches/%d9%83%d8%b1%d9%8a%d8%b3%d8%aa%d8%a7%d9%84-%d8%a8%d8%a7%d9%84%d8%a7%d8%b3-%d9%88-%d8%a3%d8%b1%d8%b3%d9%86%d8%a7%d9%84/"],
-    homeTeam: {
-      name: "Crystal Palace",
-      logo: "logos/crystal-palace.png"
+      name: "Paris Saint-Germain",
+      logo: "logos/psg.png"
     },
     awayTeam: {
       name: "Arsenal",
       logo: "logos/arsenal.png"
-    }
-  },
-  {
-    id: 3,
-    league: "England, English Premier League",
-    time: "9:00 PM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    servers: ["https://www.livelist.live/matches/%d9%81%d9%88%d9%84%d9%87%d8%a7%d9%85-%d9%88-%d9%86%d9%8a%d9%88%d9%83%d8%a7%d8%b3%d9%84-%d9%8a%d9%88%d9%86%d8%a7%d9%8a%d8%aa%d8%af/"],
-    homeTeam: {
-      name: "Fulham",
-      logo: "logos/fulham.png"
-    },
-    awayTeam: {
-      name: "Newcastle United",
-      logo: "logos/newcastle.png"
-    }
-  },
-  {
-    id: 4,
-    league: "England, English Premier League",
-    time: "9:00 PM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    servers: ["https://www.livelist.live/matches/%d9%84%d9%8a%d9%81%d8%b1%d8%a8%d9%88%d9%84-%d9%88-%d8%a8%d8%b1%d9%8a%d9%86%d8%aa%d9%81%d9%88%d8%b1%d8%af/"],
-    homeTeam: {
-      name: "Liverpool",
-      logo: "logos/liverpool.png"
-    },
-    awayTeam: {
-      name: "Brentford",
-      logo: "logos/brentford.png"
-    }
-  },
-  {
-    id: 5,
-    league: "England, English Premier League",
-    time: "9:00 PM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    servers: ["https://www.livelist.live/matches/%d9%85%d8%a7%d9%86%d8%b4%d8%b3%d8%aa%d8%b1-%d8%b3%d9%8a%d8%aa%d9%8a-%d9%88-%d8%a3%d8%b3%d8%aa%d9%88%d9%86-%d9%81%d9%8a%d9%84%d8%a7/"],
-    homeTeam: {
-      name: "Manchester City",
-      logo: "logos/man-city.png"
-    },
-    awayTeam: {
-      name: "Aston Villa",
-      logo: "logos/aston-villa.png"
-    }
-  },
-  {
-    id: 6,
-    league: "England, English Premier League",
-    time: "9:00 PM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    servers: ["https://www.livelist.live/matches/%d8%aa%d9%88%d8%aa%d9%86%d9%87%d8%a7%d9%85-%d9%87%d9%88%d8%aa%d8%b3%d8%a8%d8%b1-%d9%88-%d8%a5%d9%8a%d9%81%d8%b1%d8%aa%d9%88%d9%86/"],
-    homeTeam: {
-      name: "Tottenham Hotspur",
-      logo: "logos/tottenham.png"
-    },
-    awayTeam: {
-      name: "Everton",
-      logo: "logos/everton.png"
     }
   }
 ];
