a3212e81b1cbc6d242b02377197003629069a2aa
Anisur Rahman <anisrahman.contact@gmail.com>
2026-05-30 11:21:53 +0600

    chore(matches): admin update (1 matches)

--- public/matches.js
+++ public/matches.js
@@ -6,7 +6,7 @@ const MATCHES_DATA = [
     status: "",
     day: "today",
     isImportant: true,
-    servers: ["https://yallalive.sx/matches/psg-vs-arsenal/","https://www.hdflive.com/matches/barys-vs-arsn/","https://www.yalla-goal.com/matches/barys-vs-arsn/"],
+    servers: ["https://yallalive.sx/matches/psg-vs-arsenal/","https://www.hdflive.com/matches/barys-vs-arsn/","https://www.yalla-goal.com/matches/barys-vs-arsn/","https://www.yallashoot-plus.plus/matches/paris-sg-vs-arsenal/"],
     homeTeam: {
       name: "Paris Saint-Germain",
       logo: "logos/psg.png"
