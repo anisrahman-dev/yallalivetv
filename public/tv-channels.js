2545c2ab01998711fa7249fca30d5689ca54e612
Anisur Rahman <anisrahman.contact@gmail.com>
2026-06-11 00:42:29 +0600

    chore(tv): channel servers updated (1 server links)

--- public/tv-channels.js
+++ public/tv-channels.js
@@ -2,7 +2,7 @@ window.TV_CHANNELS = [
   {
     id: 1,
     title: "TV 1",
-    servers: ["https://test-streams.mux.dev/test_001/stream.m3u8", "https://test-streams.mux.dev/test_001/stream.m3u8", "https://test-streams.mux.dev/test_001/stream.m3u8"]
+    servers: ["", "https://test-streams.mux.dev/test_001/stream.m3u8", ""]
   },
   {
     id: 2,
