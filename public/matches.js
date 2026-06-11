da312c7b4aa0f1e42af4129933060b7287b1c07b
Anisur Rahman <anisrahman.contact@gmail.com>
2026-06-11 10:51:23 +0600

    chore(matches): admin update (2 matches)

--- public/matches.js
+++ public/matches.js
@@ -1,2 +1,36 @@
 const MATCHES_DATA = [
+  {
+    id: 1,
+    league: "FIFA World Cup 2026",
+    time: "1:00 AM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["#"],
+    homeTeam: {
+      name: "Mexico",
+      logo: "logos/mexico.png"
+    },
+    awayTeam: {
+      name: "S",
+      logo: "logos/cameroon.svg"
+    }
+  },
+  {
+    id: 2,
+    league: "FIFA World Cup 2026",
+    time: "8:00 AM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["#"],
+    homeTeam: {
+      name: "South Korea",
+      logo: "logos/south-korea.png"
+    },
+    awayTeam: {
+      name: "Czech Republic",
+      logo: "logos/czech-republic.png"
+    }
+  }
 ];
