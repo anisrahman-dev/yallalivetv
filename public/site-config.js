40954d0b793f308ecc9054430ac776c296e2cdfd
Anisur Rahman <anisrahman.contact@gmail.com>
2026-06-12 12:27:08 +0600

    chore(config): site settings updated (ads:on, vignette:on, multi:on, popunder:on, countdown:off)

--- public/site-config.js
+++ public/site-config.js
@@ -1,11 +1,11 @@
 window.SITE_CONFIG = {
-  "adsEnabled": false,
-  "vignetteEnabled": false,
-  "multiAdEnabled": false,
-  "popUnderEnabled": false,
-  "networkTagsEnabled": false,
-  "tvBannerAdsEnabled": false,
-  "tvNativeAdEnabled": false,
+  "adsEnabled": true,
+  "vignetteEnabled": true,
+  "multiAdEnabled": true,
+  "popUnderEnabled": true,
+  "networkTagsEnabled": true,
+  "tvBannerAdsEnabled": true,
+  "tvNativeAdEnabled": true,
   "countdownEnabled": false,
   "countdownSeconds": 0
 };
