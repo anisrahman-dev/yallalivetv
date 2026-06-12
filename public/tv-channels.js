4bd91b3c62578ffc0f122487e0b6f4f4bf5bcee0
Anisur Rahman <anisrahman.contact@gmail.com>
2026-06-12 23:53:56 +0600

    chore(tv): channel servers updated (6 server links)

--- public/tv-channels.js
+++ public/tv-channels.js
@@ -2,7 +2,7 @@ window.TV_CHANNELS = [
   {
     id: 1,
     title: "TV 1",
-    servers: ["https://muc001.myturn1.top:8088/live/webcricfifa1/playlist.m3u8?vidictid=206162087616&id=124249&pk=b69ee33acba9b950d6bb47458b3b3438f58ff512b2106af2cbaf6583e26dd38dbcdf10f5169624aa5fb5f0b2035c0a31dabe2add529e727b6fb476a6f3b94920", "https://muc001.myturn1.top:8088/live/webcricfifa1/playlist.m3u8?vidictid=206162087616&id=124249&pk=b69ee33acba9b950d6bb47458b3b3438f58ff512b2106af2cbaf6583e26dd38dbcdf10f5169624aa5fb5f0b2035c0a31dabe2add529e727b6fb476a6f3b94920", "https://muc001.myturn1.top:8088/live/webcricfifa1/playlist.m3u8?vidictid=206162087616&id=124249&pk=b69ee33acba9b950d6bb47458b3b3438f58ff512b2106af2cbaf6583e26dd38dbcdf10f5169624aa5fb5f0b2035c0a31dabe2add529e727b6fb476a6f3b94920"]
+    servers: ["https://mut001.myturn1.top:8088/live/webcricfifa1/playlist.m3u8?vidictid=206162929241&id=124249&pk=b69ee33acba9b950d6bb47458b3b3438f58ff512b2106af2cbaf6583e26dd38dbcdf10f5169624aa5fb5f0b2035c0a31dabe2add529e727b6fb476a6f3b94920", "https://mut001.myturn1.top:8088/live/webcricfifa1/playlist.m3u8?vidictid=206162929241&id=124249&pk=b69ee33acba9b950d6bb47458b3b3438f58ff512b2106af2cbaf6583e26dd38dbcdf10f5169624aa5fb5f0b2035c0a31dabe2add529e727b6fb476a6f3b94920", "https://mut001.myturn1.top:8088/live/webcricfifa1/playlist.m3u8?vidictid=206162929241&id=124249&pk=b69ee33acba9b950d6bb47458b3b3438f58ff512b2106af2cbaf6583e26dd38dbcdf10f5169624aa5fb5f0b2035c0a31dabe2add529e727b6fb476a6f3b94920"]
   },
   {
     id: 2,
