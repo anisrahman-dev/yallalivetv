27c9a11419fcead8adf07b7bfb27cfbefa4c3204
Anisur Rahman <anisrahman.contact@gmail.com>
2026-06-13 22:45:25 +0600

    chore(matches): admin update (5 matches)

--- public/matches.js
+++ public/matches.js
@@ -29,8 +29,59 @@ const MATCHES_DATA = [
       logo: "logos/germany.png"
     },
     awayTeam: {
-      name: "Ecuador",
+      name: "Curacao",
       logo: "logos/ecuador.png"
+    }
+  },
+  {
+    id: 3,
+    league: "FIFA World Cup 2026",
+    time: "1:00 AM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["#"],
+    homeTeam: {
+      name: "Qatar",
+      logo: "logos/qatar.png"
+    },
+    awayTeam: {
+      name: "Switzerland",
+      logo: "logos/switzerland.png"
+    }
+  },
+  {
+    id: 4,
+    league: "FIFA World Cup 2026",
+    time: "7:00 AM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["#"],
+    homeTeam: {
+      name: "Haiti",
+      logo: "logos/bournemouth.svg"
+    },
+    awayTeam: {
+      name: "Scotland",
+      logo: "logos/cameroon.svg"
+    }
+  },
+  {
+    id: 5,
+    league: "FIFA World Cup 2026",
+    time: "10:00 AM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["#"],
+    homeTeam: {
+      name: "Australia",
+      logo: "logos/australia.png"
+    },
+    awayTeam: {
+      name: "Turkey",
+      logo: "logos/turkey.png"
     }
   }
 ];
